7daf4f18c619ca9cfec781d8598e719168a275f9
Chandan Tiwary <ctiwary36@gmail.com>
2021-06-21 02:40:04 +0530

    Added:Ability_to_allow_the_playe_to_choose_a_letter_X_or_O

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,4 +1,5 @@
 package tictactoegame;
+import java.util.Scanner;
 
 public class TicTacToeGame {
     private static char[] element;
@@ -6,6 +7,7 @@ public class TicTacToeGame {
     public static void main(String[] args) {
         System.out.println("Welcome to Tic Tac Toe Game");
         board();
+        userinput();
     }
 
    	/*Create a board of char[] of size 10
@@ -14,8 +16,24 @@ public class TicTacToeGame {
 
     private static void board() {
         element = new char[10];
-        for (int i = 1; i < 10; i++) {
-            element[i] = '.';
+        for(int i=1; i<10; i++) {
+            element[i]='.';
         }
+    }
+    private static void userinput() {
+        Scanner in=new Scanner(System.in);
+
+        //Mark Selection
+        System.out.println(" Select Your Marker (O or X): ");
+
+        char usermark=in.next().charAt(0);
+        char computermark;
+        if (usermark=='X') {
+            computermark='O';
+            System.out.println("User mark is :" +usermark);}
+
+        else
+            computermark='X';
+        System.out.println("User mark is :" +usermark);
     }
 }
